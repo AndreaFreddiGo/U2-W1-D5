4778e4e36dd5e35cddd6ccc6c925ea860a2c3fbe
Andrea Freddi <56250515+AndreaFreddiGo@users.noreply.github.com>
2024-11-02 23:56:19 +0200

    Adds eventListener navbar

--- assets/script/medium.js
+++ assets/script/medium.js
@@ -1,1 +1,13 @@
+const navbar = document.getElementsByTagName('nav')[0]
+const button = document.querySelectorAll('nav button')[0]
 
+window.addEventListener('scroll', (e) => {
+  if (window.scrollY > 400) {
+    navbar.classList.add('whiteNav')
+    button.classList.add('greenButton')
+  } else {
+    navbar.classList.remove('whiteNav')
+    button.classList.remove('greenButton')
+  }
+  console.log(e)
+})
