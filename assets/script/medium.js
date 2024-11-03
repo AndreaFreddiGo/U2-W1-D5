881b89870162cafaaaacd6e01403e46f2cd59478
Andrea Freddi <56250515+AndreaFreddiGo@users.noreply.github.com>
2024-11-03 23:53:42 +0200

    Adds final version (with comments)

--- assets/script/medium.js
+++ assets/script/medium.js
@@ -1,6 +1,10 @@
+// vado a "targhettare" la navbar ed il bottone al suo interno
 const navbar = document.getElementsByTagName('nav')[0]
 const button = document.querySelectorAll('nav button')[0]
 
+// aggiungo un listener che a seguito di scroll, quando raggiungo una certa
+// profondità di scorrimento verticale, aggiunge le rispettive classi a bottone
+// e navbar che ne cambiano il background color con un fade-in
 window.addEventListener('scroll', (e) => {
   if (window.scrollY > 400) {
     navbar.classList.add('whiteNav')
